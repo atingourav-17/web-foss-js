52aa890043b5ba77fe49805a2ff55608b102dad3
atingourav-17 <atingourav@gmail.com>
2025-11-15 19:32:48 +0530

    Changing Background Color on Button Click

---
+++ LCB2025036/button.js
@@ -0,0 +1,7 @@
+function changecolor (){
+    let button = document.getElementById('button');
+    button.style.backgroundColor="turquoise";
+    let hii = document.getElementById('hii');
+    hii.style.backgroundColor="black";
+    hii.style.color="white";
+};
